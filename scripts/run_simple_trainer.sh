28e8c6fabc3ccbd211a3519a087c3b36c3826e95
Ubuntu <ubuntu@ip-172-31-33-174.us-east-2.compute.internal>
2025-11-20 16:31:51 +0000

    Cleanup

--- scripts/run_simple_trainer.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python /home/ubuntu/xr-gs/gsplat/examples/simple_trainer.py default \
-    --data_dir /home/ubuntu/xr-gs/data/nerf_llff_data/fern_xrgs --data_factor 1 \
-    --result_dir /home/ubuntu/xr-gs/data/nerf_llff_data/fern_xrgs/$1
